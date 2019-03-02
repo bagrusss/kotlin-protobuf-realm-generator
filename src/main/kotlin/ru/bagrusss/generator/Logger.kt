be509c3b0432b4057379787ddf50bc6cdf534fa1
Vladislav Kozhushko <bagrusss@gmail.com>
2019-03-02 17:22:10 +0300

    clean

--- src/main/kotlin/ru/bagrusss/generator/Logger.kt
+++ src/main/kotlin/ru/bagrusss/generator/Logger.kt
@@ -11,14 +11,13 @@ object Logger {
     @JvmField val logPath = System.getProperty("user.dir") + File.separator
     private lateinit var path: Path
 
-    private lateinit var currentFile: String
-
     fun prepare(fileName: String) {
-        currentFile = "$logPath$fileName"
+        val currentFile = "$logPath$fileName"
         path = Paths.get(currentFile)
-        val log = File(currentFile)
-        log.delete()
-        log.createNewFile()
+        File(currentFile).apply {
+            delete()
+            createNewFile()
+        }
     }
 
     fun log(string: String) {
